95126f9ca47b6a3cf77090495611eeb49718bb7e
Louisa Goncharenko <louisa.gonch@gmail.com>
2024-02-06 17:02:39 -0500

    project set up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Tetris Redux</h1>
+      </header>
     </div>
   );
 }
