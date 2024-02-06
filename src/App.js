e9690751f0e0510f1ceb8f9969f3f5044bc0b380
Louisa Goncharenko <louisa.gonch@gmail.com>
2024-02-06 17:34:53 -0500

    Added grid board"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import GridSquare from './components/GridSquare/GridSquare'
+import GridBoard from './components/GridBoard/GridBoard';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
-      <GridSquare color="1" />
+      <GridBoard />
     </div>
   );
 }
---
+++ src/components/GridBoard/GridBoard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import GridSquare from '../GridSquare/GridSquare'
+
+// Represents a 10 x 18 grid of grid squares
+
+export default function GridBoard() {
+  // generates an array of 18 rows, each containing 10 GridSquares.
+  const grid = []
+  for (let row = 0; row < 18; row ++) {
+    grid.push([])
+    for (let col = 0; col < 10; col ++) {
+      grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
+    }
+  }
+  return (
+    <div className='grid-board'>
+      {grid}
+    </div>
+  )
+}
