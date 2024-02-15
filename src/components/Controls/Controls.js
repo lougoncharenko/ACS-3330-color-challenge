f6d5b86f7b90770576c2ce98449fa32b607407bb
Louisa Goncharenko <louisa.gonch@gmail.com>
2024-02-15 16:14:58 -0500

    timer created

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,42 +1,80 @@
-import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import {moveDown, moveLeft, moveRight, rotate} from '../../redux/gameSlice'
-import './Controls.css'
+import React, { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { moveDown, moveLeft, moveRight, rotate } from "../../redux/gameSlice";
+import "./Controls.css";
 
 export default function Controls(props) {
-  const { isRunning } = useSelector(state => state)
-  const dispatch = useDispatch()
+  const { isRunning, speed } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  // Set up the game timer
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+
+  // Initialize request animation frame and remove it when isRunning changes
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
+
+  // Handle game updates to move blocks down the screen
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+    lastUpdateTimeRef.current = time;
+  };
   return (
-  <div className="controls">
-    
-    <button 
-    className="control-button" 
-    disabled={!isRunning}
-    onClick={(e) => {
-      dispatch(moveLeft())
-    }}>Left</button>
+    <div className="controls">
+      <button
+        className="control-button"
+        disabled={!isRunning}
+        onClick={(e) => {
+          dispatch(moveLeft());
+        }}
+      >
+        Left
+      </button>
 
-    <button 
-    className="control-button" 
-    disabled={!isRunning}
-    onClick={(e) => {
-      dispatch(moveRight())
-    }}>Right</button>
+      <button
+        className="control-button"
+        disabled={!isRunning}
+        onClick={(e) => {
+          dispatch(moveRight());
+        }}
+      >
+        Right
+      </button>
 
-    <button 
-    className="control-button" 
-    disabled={!isRunning}
-    onClick={(e) => {
-      dispatch(rotate())
-    }}>Rotate</button>
+      <button
+        className="control-button"
+        disabled={!isRunning}
+        onClick={(e) => {
+          dispatch(rotate());
+        }}
+      >
+        Rotate
+      </button>
 
-    <button 
-    className="control-button" 
-    disabled={!isRunning}
-    onClick={(e) => {
-      dispatch(moveDown())
-    }}>Down</button>
-
-  </div>
-  )
+      <button
+        className="control-button"
+        disabled={!isRunning}
+        onClick={(e) => {
+          dispatch(moveDown());
+        }}
+      >
+        Down
+      </button>
+    </div>
+  );
 }
